85e8c0bbc60687d1ae7202a627022e0637a359ef
Felipe Galvao <contato@felipegalvao.com.br>
2016-11-29 23:28:57 -0200

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load foundation
+$(document).foundation();
+
+// Load app.css
+require('style!css!sass!applicationStyles');
+
+
+ReactDOM.render(
+  <p>Boilerplate 3 Project</p>,
+  document.getElementById('app')
+);
